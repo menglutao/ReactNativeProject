34e369baddfe327c4553495da357cd8937e96b13
陶梦露 <taomenglu@taomengludeMacBook-Pro.local>
2022-08-27 18:28:21 +0200

    Write to and read from Firebase realtime database

--- my-project/App.js
+++ my-project/App.js
@@ -2,19 +2,44 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 import * as Location from 'expo-location';
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore/lite';
-// import firebaseConfig from './firebase_config.json';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase_config.json';
+import { getDatabase, ref, set , child, get} from "firebase/database";
 
 
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
+// initialize firebase 
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+
+//write User's data into firebase
+const writeUserData = (userId, name, email) => {
+  set(ref(db,'users/' + userId), {
+    userId: userId,
+    name: name,
+    email: email,
+    
+    });
+}
+
+//read the db data for once
+const getUserData = (userId) => {
+  const dbRef = ref(db);
+  get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+}
 
 const Separator = () => (
   <View style={styles.separator} />
 );
 
-const getCurrentLocation = async() => {
+const updateCurrentLocation = async() => {
   
   let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -24,6 +49,7 @@ const getCurrentLocation = async() => {
   
   let location = await Location.getCurrentPositionAsync({});
   Alert.alert(JSON.stringify(location));
+  
   console.log(location);
   // Alert.alert('You tapped the button!');
 }
@@ -36,7 +62,8 @@ const App = () => (
       </Text>
       <Button
         title="Current Location"
-        onPress={getCurrentLocation}
+        color="#fc4903"
+        onPress={updateCurrentLocation}
       />
     </View>
     <Separator />
@@ -45,20 +72,20 @@ const App = () => (
         Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
       </Text>
       <Button
-        title="Press me"
-        color="#f194ff"
-        onPress={() => Alert.alert('Button with adjusted color pressed')}
+        title="Write User Data"
+        color="#fc8003"
+        onPress={() => writeUserData('001', 'John Doe', 'azraeltml@gmail.com')}
       />
     </View>
     <Separator />
     <View>
       <Text style={styles.title}>
-        All interaction for the component are disabled.
+        Read user data by userId
       </Text>
       <Button
         title="Press me"
-        disabled
-        onPress={() => Alert.alert('Cannot press this one')}
+        color="#fcc203"
+        onPress={() => getUserData('001')}
       />
     </View>
     <Separator />
@@ -69,10 +96,12 @@ const App = () => (
       <View style={styles.fixToText}>
         <Button
           title="Left button"
+          color="#d1ceb0"
           onPress={() => Alert.alert('Left button pressed')}
         />
         <Button
           title="Right button"
+          color="#d1ceb0"
           onPress={() => Alert.alert('Right button pressed')}
         />
       </View>
