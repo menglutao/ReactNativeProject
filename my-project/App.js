6e27c1e3f060cbf8d54b6b47ad24fd82ff1974b1
menglutao <azraeltml@gmail.com>
2022-08-28 00:03:05 +0200

    Add input text area for update location and tract location

--- my-project/App.js
+++ my-project/App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert,TextInput } from 'react-native';
 import * as Location from 'expo-location';
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from './firebase_config.json';
 import { getDatabase, ref, set , child, get} from "firebase/database";
 
 
+
 // initialize firebase 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
-const currentUserName = "Meng";
+// const currentUserName = "Meng";
+
+// const trackedUserName = "Daniel";
 
 //write User's data into firebase
 const writeUserData = (userId,position) => {
@@ -39,7 +42,7 @@ const Separator = () => (
   <View style={styles.separator} />
 );
 
-const updateCurrentLocation = async() => {
+const updateMyCurrentLocation = async(currentUserName) => {
   
   let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -55,63 +58,87 @@ const updateCurrentLocation = async() => {
   // Alert.alert('You tapped the button!');
 }
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
-      </Text>
-      <Button
-        title="Current Location"
-        color="#fc4903"
-        onPress={updateCurrentLocation}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
-      </Text>
-      <Button
-        title="Write User Data"
-        color="#fc8003"
-        onPress={() => Alert.alert('User data not written')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        Read user data by userId
-      </Text>
-      <Button
-        title="Press me"
-        color="#fcc203"
-        onPress={() => getUserData('001')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          color="#d1ceb0"
-          onPress={() => Alert.alert('Left button pressed')}
+const App = () => {
+
+  const [currentUserName, onChangeCurrentUserName] = React.useState("Meng"); 
+  const [trackedUserName, onChangeTrackedUserName] = React.useState("Daniel");
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeCurrentUserName}
+          value={currentUserName}
         />
         <Button
-          title="Right button"
-          color="#d1ceb0"
-          onPress={() => Alert.alert('Right button pressed')}
+          title="Update My Current Location"
+          color="#fc4903"
+          onPress={() => updateMyCurrentLocation(currentUserName)}
         />
       </View>
-    </View>
-    <StatusBar style="auto" />
-  </SafeAreaView>
-);
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
+        </Text>
+        <Button
+          title="Write User Data"
+          disabled
+          color="#fc8003"
+          onPress={() => Alert.alert('User data not written')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          Read user data by userId
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeTrackedUserName}
+          value={trackedUserName}
+        />
+
+        <Button
+          title="Track Your Love"
+          color="#fcc203"
+          onPress={() => getUserData(trackedUserName)}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+          This layout strategy lets the title define the width of the button.
+        </Text>
+        <View style={styles.fixToText}>
+          <Button
+            title="Left button"
+            color="#d1ceb0"
+            onPress={() => Alert.alert('Left button pressed')}
+          />
+          <Button
+            title="Right button"
+            color="#d1ceb0"
+            onPress={() => Alert.alert('Right button pressed')}
+          />
+        </View>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
